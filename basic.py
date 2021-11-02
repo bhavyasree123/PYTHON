b7c51d6ce175db1ddf6915fd9e249bac4c0411dd
bhavyasree123 <74817208+bhavyasree123@users.noreply.github.com>
2021-11-02 23:12:37 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,13 @@
+a1=1
+a2=3
+tpe = print(a1-a2-a1)
+print(a1+a2)
+
+print(a1-a2)
+
+type(print(a1-a2-a1))
+
+print(a1==a2)
+print(a1/a2/a1)
+
+type(a1)
---
+++ start.py
@@ -0,0 +1,3 @@
+name1 = input("enter your fname")
+name2 = input("enter your sname")
+print("this is your name" ,name1,name2)
